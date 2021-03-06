d77e8919f1327abb018b379fd961e24087ce922e
Fabs <faebeee@gmail.com>
2021-03-06 10:46:46 +0100

    Add new build

--- out/cli/cli.js
+++ out/cli/cli.js
@@ -4,4 +4,4 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var index_1 = require("../index");
 process.env.SLACK_WEBHOOK_URL && index_1.addReporter(index_1.slackReporter(process.env.SLACK_WEBHOOK_URL));
 process.env.DD_CLIENT_API_KEY && index_1.addReporter(index_1.datadogReporter(process.env.DD_CLIENT_API_KEY));
-index_1.overwatch([], './configs/projects/**/*.js', './configs/test-cases/**/*.js');
+index_1.overwatch([], './example/projects/**/*.js', './example/test-cases/**/*.js');
